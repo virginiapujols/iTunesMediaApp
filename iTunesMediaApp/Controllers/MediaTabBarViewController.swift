9ac4e63a1115c9a5edc5859fdfda470096293aff
virginiapujols <virginia.pujols.ti@gmail.com>
2019-04-11 20:57:31 -0400

    Changing root to Tabbar to display different media types. Added tableview with simple cell to display the names.

---
+++ iTunesMediaApp/Controllers/MediaTabBarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MediaTabBarViewController.swift
+//  iTunesMediaApp
+//
+//  Created by Virginia Pujols on 4/11/19.
+//  Copyright © 2019 Virginia Pujols. All rights reserved.
+//
+
+import UIKit
+
+class MediaTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
